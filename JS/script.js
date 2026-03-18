dd31005652895f72506f0f0eccec28e5401b292b
Isaac Young <isaac3.young6@gmail.com>
2026-03-18 09:27:28 -0700

    style: base styling done
    background as been made on the hero, nav switched to fixed so it goes down the entire scroll, timeline divider placed, some very basic styling centering and gap to the cards as well as alternating sides of the divider

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const heroTitle = document.querySelector('#hero-title')
+
+const titleDisapearOnScroll = () => {
+    if (window.scrollY > 350) {
+        heroTitle.style.display = 'none'
+    }else {
+        heroTitle.style.display = 'block'
+    }
+}
+
+window.addEventListener('scroll', () => {
+    titleDisapearOnScroll()
+})
