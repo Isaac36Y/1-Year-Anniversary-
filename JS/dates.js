19c93792888db8e689bd0d4d106c29fb1f7bb71c
Isaac Young <isaac3.young6@gmail.com>
2026-03-25 15:16:23 -0700

    docs: dates moved into a seperate js file. Images from february-september have been uploaded into files in the project

---
+++ JS/dates.js
@@ -0,0 +1,58 @@
+const dates = [
+    {
+        date: "February 14th, 2025",
+        modifierClass: null,
+        title: 'Our First Date',
+        description: "We don't have any pictures together but this is the night that started it all. We sat right at that bar, I had an old fashioned, you had a tequila pineapple, the rest is the best history. I love you💙",
+        img: './Images/Dates/Feb,2025/feb14.webp'
+    },
+    {
+        date: "February 15th, 2025",
+        modifierClass: null,
+        title: 'Scary House',
+        description: "Looks like the first date went well😉 Good kisser eh?",
+        img: './Images/Dates/Feb,2025/feb15.png'
+    },
+    {
+        date: "April 18th, 2025",
+        modifierClass: 'upper-table-rock',
+        title: 'Upper Table Rock Hike',
+        description: null,
+        img: [
+            { src: 'Images/Dates/April,2025/april18.hike/april18-1.jpeg', description: "Our wine and games night. I probably won at uno😉"},
+            { src: 'Images/Dates/April,2025/april18.hike/april18-2.jpeg', description: "Our wine and games night. I probably won at uno😉"},
+            { src: 'Images/Dates/April,2025/april18.hike/april18-3.jpeg', description: "Our wine and games night. I probably won at uno😉"},
+            { src: 'Images/Dates/April,2025/april18.hike/april18-4.jpeg', description: "Our wine and games night. I probably won at uno😉"},
+            { src: 'Images/Dates/April,2025/april18.hike/april18-5.jpeg', description: "Our wine and games night. I probably won at uno😉"},
+            { src: 'Images/Dates/April,2025/april18.hike/april18-6.jpeg', description: "Our wine and games night. I probably won at uno😉"},
+            { src: 'Images/Dates/April,2025/april18.hike/april18-7.jpeg', description: "Our wine and games night. I probably won at uno😉"}
+        ]
+    },
+    {
+        date: "June 28th, 2025",
+        modifierClass: null,
+        title: 'Camping Trip',
+        description: 'Every night we froze our butts off but this night we enjoyed some wine and played games in out tent',
+        img: './Images/AC8332FB-ED4B-46E9-8CF2-6555A49C155A_1_102_o.jpeg'
+    },
+    {
+        date: "June 28th, 2025",
+        modifierClass: 'camping-trip',
+        title: 'Camping Trip',
+        description: null,
+        img: [
+            { src: './Images/2F41D630-9080-453F-B6BC-2EF8D2356533_4_5005_c.jpeg', description: "Our wine and games night. I probably won at uno😉"},
+            { src: './Images/1968CDC9-8A1F-4C70-99E4-5AD206764BAC_1_102_o.jpeg', description: "Best breakfaast a camper could ask for. Nice boots"},
+            { src: './Images/3D023E7B-40C8-45D7-97E5-29EF475C199F_1_102_o.jpeg', description: "A waterfall hike along the creek"}
+        ]
+    },
+    {
+        date: "June 28th, 2025",
+        modifierClass: null,
+        title: 'Camping Trip',
+        description: 'Every night we froze our butts off but this night we enjoyed some wine and played games in out tent',
+        img: './Images/AC8332FB-ED4B-46E9-8CF2-6555A49C155A_1_102_o.jpeg'
+    }
+]
+
+export default dates
--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,7 @@
 const heroTitle = document.querySelector('#hero-title')
 const timelineCardsContainer = document.querySelector('#timeline-cards-container');
 
-const dates = [
-    {
-        date: "September 1st, 2025",
-        modifierClass: null,
-        title: 'Your Birthday',
-        description: 'We got massages, made homemade pasta, and had the best spaghetti dinner with jalepeno bread! 26 never looked so good',
-        img: './Images/D3460AF6-40E4-46A2-9F1F-C56EFFEC0BCC_1_105_c.jpeg'
-    },
-    {
-        date: "October 15th, 2025",
-        modifierClass: null,
-        title: 'Scary House',
-        description: "I don't like haunted house very much but I had so much fun on this day with you. The pumkin patch was great and the evening was such a good memory. Even waiting in line to get into the haunted house is a great time with you.",
-        img: './Images/6EA551D6-A407-4C4E-8575-B44494C9C1E3_1_102_o.jpeg'
-    },
-    {
-        date: "April 10th, 2025",
-        modifierClass: null,
-        title: 'Upper Table Rock Hike',
-        description: "Spring sprang and so do I when I'm with you.",
-        img: './Images/48E8BFAB-6D6E-4ED2-AC1E-02439EA48A25_1_105_c.jpeg'
-    },
-    {
-        date: "June 28th, 2025",
-        modifierClass: 'camping-trip',
-        title: 'Camping Trip',
-        description: null,
-        img: [
-            { src: './Images/2F41D630-9080-453F-B6BC-2EF8D2356533_4_5005_c.jpeg', description: "Our wine and games night. I probably won at uno😉"},
-            { src: './Images/1968CDC9-8A1F-4C70-99E4-5AD206764BAC_1_102_o.jpeg', description: "Best breakfaast a camper could ask for. Nice boots"},
-            { src: './Images/3D023E7B-40C8-45D7-97E5-29EF475C199F_1_102_o.jpeg', description: "A waterfall hike along the creek"}
-        ]
-    },
-    {
-        date: "June 28th, 2025",
-        modifierClass: null,
-        title: 'Camping Trip',
-        description: 'Every night we froze our butts off but this night we enjoyed some wine and played games in out tent',
-        img: './Images/AC8332FB-ED4B-46E9-8CF2-6555A49C155A_1_102_o.jpeg'
-    }
-]
+import dates from './dates.js'
 
 const renderTimeline = () => {
     const renderedDates = dates.map(date => {
