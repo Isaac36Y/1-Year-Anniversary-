8abc64230e7360f66feea6b2124d0c7d999101d7
Isaac Young <isaac3.young6@gmail.com>
2026-03-27 14:31:58 -0700

    style: changed the p tag for the images in modifiers so the a img having a description or not doesnt effect its placement when its in a right or left stack

--- JS/dates.js
+++ JS/dates.js
@@ -4,14 +4,14 @@ const dates = [
         modifierClass: null,
         title: 'Our First Date',
         description: "We don't have any pictures together but this is the night that started it all. We sat right at that bar, I had an old fashioned, you had a tequila pineapple, the rest is the best history. I love you💙",
-        img: './Images/Dates/Feb,2025/feb14.webp'
+        img: './Images/Dates/Feb2025/feb14.webp'
     },
     {
         date: "February 15th, 2025",
         modifierClass: null,
         title: 'Scary House',
         description: "Looks like the first date went well😉 Good kisser eh?",
-        img: './Images/Dates/Feb,2025/feb15.png'
+        img: 'Images/Dates/Feb2025/feb15.jpeg'
     },
     {
         date: "April 18th, 2025",
@@ -29,21 +29,42 @@ const dates = [
         ]
     },
     {
-        date: "June 28th, 2025",
+        date: "May 2nd, 2025",
         modifierClass: null,
-        title: 'Camping Trip',
-        description: 'Every night we froze our butts off but this night we enjoyed some wine and played games in out tent',
-        img: './Images/AC8332FB-ED4B-46E9-8CF2-6555A49C155A_1_102_o.jpeg'
+        title: 'My birthday',
+        description: "We went out for dinner and watched Star Wars Revenge of the sith in theatres. A really great date night and such a nice gift",
+        img: 'Images/Dates/May2025/may1.jpeg'
     },
     {
-        date: "June 28th, 2025",
+        date: "May 19th, 2025",
+        modifierClass: null,
+        title: 'AI made a family picture of us😂 ',
+        description: "",
+        img: 'Images/Dates/May2025/may19.jpeg'
+    },
+    {
+        date: "May 26th, 2025",
+        modifierClass: null,
+        title: 'One of our many lunch dates at Comal',
+        description: "",
+        img: 'Images/Dates/May2025/may26.jpeg'
+    },
+    {
+        date: "June 26th - June 29th, 2025",
         modifierClass: 'camping-trip',
         title: 'Camping Trip',
         description: null,
         img: [
-            { src: './Images/2F41D630-9080-453F-B6BC-2EF8D2356533_4_5005_c.jpeg', description: "Our wine and games night. I probably won at uno😉"},
-            { src: './Images/1968CDC9-8A1F-4C70-99E4-5AD206764BAC_1_102_o.jpeg', description: "Best breakfaast a camper could ask for. Nice boots"},
-            { src: './Images/3D023E7B-40C8-45D7-97E5-29EF475C199F_1_102_o.jpeg', description: "A waterfall hike along the creek"}
+            { src: 'Images/Dates/June2025/june26camping/camping-1.jpeg', description: ""},
+            { src: 'Images/Dates/June2025/june26camping/camping-2.jpeg', description: "So proud of your tent and you should be😚"},
+            { src: 'Images/Dates/June2025/june26camping/camping-3.jpeg', description: "It's like we know what we're doing"},
+            { src: 'Images/Dates/June2025/june26camping/camping-4.jpeg', description: "The best breakfast a camper could ask for"},
+            { src: 'Images/Dates/June2025/june26camping/camping-5.jpeg', description: ""},
+            { src: 'Images/Dates/June2025/june26camping/camping-6.jpeg', description: ""},
+            { src: 'Images/Dates/June2025/june26camping/camping-7.jpeg', description: ""},
+            { src: 'Images/Dates/June2025/june26camping/camping-8.jpeg', description: ""},
+            { src: 'Images/Dates/June2025/june26camping/camping-9.jpeg', description: ""},
+            { src: 'Images/Dates/June2025/june26camping/camping-10.jpg', description: ""}
         ]
     },
     {
